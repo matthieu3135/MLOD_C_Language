6b063394e9511c0f95d18accbb0f3d3600167ef1
matthieu3135 <matthieu.pouvreau8@orange.fr>
2022-10-12 08:43:16 +0200

    Matrices; It worksgit commit m Matrices

--- TP-02-pointeurs/Matrices.c
+++ TP-02-pointeurs/Matrices.c
@@ -1,6 +1,7 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include "Matrices.h"
+#include <stdio.h>
 #define SIZE 5
 
 
--- TP-02-pointeurs/Matrices.h
+++ TP-02-pointeurs/Matrices.h
@@ -1,12 +1,20 @@
 #include <stdint.h>
 #include <stdlib.h>
+#include <stdio.h>
 #define SIZE 5
 
 int64_t matrix_mult(int64_t res[SIZE][SIZE],int64_t m1[SIZE][SIZE],int64_t m2[SIZE][SIZE]){
-    res = 0*res;   //pas de multiplication, il faut passer par une boucle for
+    //res = 0*res;   //pas de multiplication, il faut passer par une boucle for
     for(int i = 0; i<SIZE; i++){
-        for(int k = 0; k<SIZE; k++){
-            res[i][j] = res[i][j] + m1[i][k] * m2[k][i];   // que vaut j
+        for(int j = 0; j<SIZE; j++){
+            res[i][j] = 0;
+        }  
+    }
+    for(int i = 0; i<SIZE; i++){
+        for(int j = 0; j<SIZE; j++){
+            for(int k = 0; k<SIZE; k++){
+                res[i][j] = res[i][j] + m1[i][k] * m2[k][i];   // que vaut j
+            }
         }
 
     }
@@ -15,7 +23,11 @@ int64_t matrix_mult(int64_t res[SIZE][SIZE],int64_t m1[SIZE][SIZE],int64_t m2[SI
 
 int64_t matrix_print(int64_t res[SIZE][SIZE]){
     for(int i = 0; i<SIZE; i++){
-        printf("%d\n", res[i][:]);  //pas de :
+        for(int j = 0; j<SIZE; j++){
+            printf("%d",res[i][j]);
+            printf(j<SIZE-1? "\t" : "\n"); //si j<SIZE-1 alors on met un espace, si j = SIZE (dernière valeure), alors retour à la ligne
+        }
+        //printf("%d\n", res[i][:]);  //pas de :
     }
     return 0;
 }
