097c6797fb6a525656a43d91c38f6e45f9fb0af2
matthieu3135 <matthieu.pouvreau8@orange.fr>
2022-10-12 21:03:46 +0200

    Update but Month still doesn't work

--- TP-02-pointeurs/Date.c
+++ TP-02-pointeurs/Date.c
@@ -7,21 +7,21 @@ void initialiseDate(Date* d){
     int D, Y;
     char M;
     printf("\n Which day? ");
-    scanf("%d", &D);
+    scanf("%d", &(d->Day));
     printf("\n Which month? ");
-    scanf("%s", &M);
+    scanf("%s", M);
     printf("\n Which year? ");
-    scanf("%d", &Y);
-    d->Day = D;
+    scanf("%d", &d->Year);
+    //d->Day = D;
     d->Month = M;
-    d->Year = Y;
-    printf("\n %d %d %d\n", D, M, Y);
+    //d->Year = Y;
+    //printf("\n %d %d %d\n", D, M, Y);
     printf("\n %d %d %d\n", d->Day, d->Month, d->Year);
 
 }
 
 void afficheDate(Date* d){
-    printf("The date you have selected is %d  %d  %d\n", *(d->Day),d->Month, d->Year);
+    printf("The date you have selected is %d  %d  %d\n", d->Day,d->Month, d->Year);
 }
 
 int main(){
@@ -31,6 +31,8 @@ int main(){
     //enum Month_of_year mois;
     //mois = January;
     //printf("\n%d\n", mois);
+    d.Month = January;
+    printf("%d \n", d.Month);
 
     initialiseDate(&d);
     afficheDate(&d);
